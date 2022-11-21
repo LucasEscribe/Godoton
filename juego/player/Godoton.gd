a0bbf6de4ad752cb8435417b8174020ec452c4af
LucasEscribe <lfd.meneses@gmail.com>
2022-11-21 16:29:46 -0300

    Dando movimiento
    Al personaje principal

---
+++ juego/player/Godoton.gd
@@ -0,0 +1,11 @@
+class_name Godoton
+extends KinematicBody
+
+
+## Atributos Export
+export var gravedad: float = 9.8
+
+## Atributos
+const direccion_arriba: Vector3 = Vector3.UP
+var movimiento: Vector3 = Vector3.ZERO
+
