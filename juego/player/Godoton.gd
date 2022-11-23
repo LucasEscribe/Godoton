848b63c14657e5224271c71344c7d91bedc1ada4
LucasEscribe <lfd.meneses@gmail.com>
2022-11-23 10:58:37 -0300

    Semana 2
    Clase 2.4 todo OK
    Salto

--- juego/player/Godoton.gd
+++ juego/player/Godoton.gd
@@ -6,26 +6,48 @@ extends KinematicBody
 export var velocidad_max: Vector2 = Vector2(10.0, 60.0)
 export var gravedad: float = 9.8
 export var impulso: float = 50.0
+export var fuerza_salto: float = 21.0
 
 ## Atributos Onready
 onready var brazo_camara: SpringArm = $BrazoCamara
 
-## Atributos
+## Atributos Constantes
 const direccion_arriba: Vector3 = Vector3.UP
+
+## Atributos Variables
 var movimiento: Vector3 = Vector3.ZERO
+var saltando: bool
+var saltando_interrumpido: bool
 
 
 ## Métodos
 func _process(delta: float) -> void:
 	brazo_camara.translation = translation
+	#move_and_slide(movimiento, direccion_arriba)
+	
 
 func _physics_process(delta: float) -> void:
 	movimiento_vertical()
-	move_and_slide(movimiento, direccion_arriba)
-	if Input.is_action_pressed("saltar") and saltando and not saltando_interrumpido:
-		movimiento.y += fuerza_salto
+	movimiento = move_and_slide(movimiento, direccion_arriba)
 
+	
 func movimiento_vertical() -> void:
 	if not is_on_floor():
-		movimiento.y -= gravedad
 		movimiento.y = clamp(movimiento.y, -velocidad_max.y, impulso)
+		movimiento.y -= gravedad
+		if Input.is_action_just_released("saltar"):
+			saltando_interrumpido = true
+		else:
+			saltando = false
+		
+		var inicio_salto: bool = is_on_floor() and Input.is_action_just_pressed("saltar")
+		if inicio_salto:
+			saltando = true
+			saltando_interrumpido = false
+		
+		if movimiento.y > velocidad_max.y:
+			saltando_interrumpido = true
+
+		if Input.is_action_pressed("saltar") and saltando and not saltando_interrumpido:
+			movimiento.y += fuerza_salto
+		
