9b3184bf13d6d17963d35561d31253403aede82e
LucasEscribe <lfd.meneses@gmail.com>
2022-11-27 11:04:33 -0300

    Viaje

--- juego/player/BrazoCamara.gd
+++ juego/player/BrazoCamara.gd
@@ -4,8 +4,9 @@ extends SpringArm
 ## Atributos Exports
 export var velocidad_mov_camara: float = 0.10
 export var rango_rotacion_camara_x: Vector2 = Vector2(-90.0, 30.0)
-export var rango_rotacion_camara_y: Vector2 = Vector2(00.0, 360.0) 
-
+export var rango_rotacion_camara_y: Vector2 = Vector2(0.0, 360.0)
+export var camara_y_invertida: bool = false
+export var camara_x_invertida: bool = false
 
 
 ## Métodos
@@ -17,7 +18,7 @@ func _ready() -> void:
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
 		# Movimiento alrededor del eje X (arriba/abajo)
-		rotation_degrees.x += event.relative.y * velocidad_mov_camara
+		rotation_degrees.x += event.relative.y * velocidad_mov_camara.y
 		rotation_degrees.x = clamp(
 			rotation_degrees.x,
 			rango_rotacion_camara_x.x,
@@ -25,11 +26,11 @@ func _unhandled_input(event: InputEvent) -> void:
 		)
 
 		# Movimiento alrededor del eje Y (derecha/izquierda)
-		rotation_degrees.y += event.relative.x * velocidad_mov_camara
+		rotation_degrees.y += event.relative.x * velocidad_mov_camara.y
 		rotation_degrees.x = wrapf(
 			rotation_degrees.y,
 			rango_rotacion_camara_y.x,
-			rango_rotacion_camara_y.y			
+			rango_rotacion_camara_y.y
 		)
 		
 	#TODO: Sólo para Debug
--- juego/player/Godoton.gd
+++ juego/player/Godoton.gd
@@ -6,8 +6,8 @@ extends KinematicBody
 export var velocidad_max: Vector2 = Vector2(10.0, 60.0)
 export var gravedad: float = 9.8
 export var impulso: float = 50.0
-export var fuerza_salto: float = 21.0
-
+export var fuerza_salto: float = 18.0
+	
 ## Atributos Onready
 onready var brazo_camara: SpringArm = $BrazoCamara
 
@@ -16,31 +16,32 @@ const direccion_arriba: Vector3 = Vector3.UP
 
 ## Atributos Variables
 var movimiento: Vector3 = Vector3.ZERO
-var saltando: bool
-var saltando_interrumpido: bool
+var saltando: bool = false
+var saltando_interrumpido: bool = false
 
 
 ## Métodos
 func _process(delta: float) -> void:
 	brazo_camara.translation = translation
-	#move_and_slide(movimiento, direccion_arriba)
-	
+
 
 func _physics_process(delta: float) -> void:
+	movimiento_horizontal()
 	movimiento_vertical()
 	movimiento = move_and_slide(movimiento, direccion_arriba)
 
 	
 func movimiento_vertical() -> void:
 	if not is_on_floor():
-		movimiento.y = clamp(movimiento.y, -velocidad_max.y, impulso)
 		movimiento.y -= gravedad
+		movimiento.y = clamp(movimiento.y, -velocidad_max.y, impulso)
 		if Input.is_action_just_released("saltar"):
 			saltando_interrumpido = true
 		else:
 			saltando = false
 		
 		var inicio_salto: bool = is_on_floor() and Input.is_action_just_pressed("saltar")
+
 		if inicio_salto:
 			saltando = true
 			saltando_interrumpido = false
@@ -51,3 +52,13 @@ func movimiento_vertical() -> void:
 		if Input.is_action_pressed("saltar") and saltando and not saltando_interrumpido:
 			movimiento.y += fuerza_salto
 		
+func movimiento_horizontal() -> void:
+	movimiento.x = tomar_direccion().x * velocidad_max.x
+	movimiento.z = tomar_direccion().z * velocidad_max.x
+	
+func tomar_direccion()  -> Vector3:
+	var direccion: Vector3 = Vector3.ZERO
+	direccion.x = Input.get_action_strength("mov_derecha") - Input.get_action_strength("mov_izquierda")
+	direccion.z = Input.get_action_strength("mov_atras") - Input.get_action_strength("mov_adelante")
+	
+	return direccion 
